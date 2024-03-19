be7fefd00237fe2cd921da91ecb03043a19fd448
Bilguun <mandahb150@gmail.com>
2024-03-19 01:25:03 -0700

    finance modulruu item add hiideg bolgono

--- app.js
+++ app.js
@@ -23,20 +23,24 @@ var uiController = (function () {
 })();
 
 // sanhuutei ajillah controller
+// dald ogogdol bogood shuud handah bolomjgui private data
 var financeController = (function () {
+  // private data
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
+  // private data
   var Expense = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
+  // private data
   var data = {
-    allItems: {
+    items: {
       inc: [],
       exp: [],
     },
@@ -47,15 +51,40 @@ var financeController = (function () {
     },
   };
 
-    
-  
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+      
+          if (data.items[type].length === 0) id = 1;
+          
+          else {
+              id = data.items[type][data.items[type].length - 1].id + 1;
+          }
+
+      if (type === "inc") {
+        item = new Income(id, desc, value);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.items[type].push(item);
+      },
+      
+      seeData: function () {
+          return data;
+      }
+  };
 })();
 
 // program holbogch controller
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     // 1.oruulah ogogdliig delgetsees olj awna
+    var input = uiController.getInput();
+
     // 2.olj awsan ogogdluudee sanhuugiin controllert damjuulj tend hadgalna
+    financeController.addItem(input.type, input.description, input.value);
+
     // 3. olj awsan ogogdluudee web deeree tohiroh hesegt ni gargana
     // 4.toswiig tootsoolno
     // 5.etssiin uldegdel tootsoog delgetsend gargana
